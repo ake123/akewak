1582edecd7165f354caba980d9433e42b8f083cb
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-07-10 13:18:30 +0000

    Built site for gh-pages

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggler = document.getElementById("theme-toggle");
-  if (toggler) {
-    toggler.addEventListener("click", () => {
-      document.body.classList.toggle("dark-mode");
-    });
-  }
-});
